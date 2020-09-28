92f1fed05f622a9e8ff6fcf5b5ddac3ffb436248
Juan <juandamaso94@gmail.com>
2020-09-28 21:16:14 +0200

    Tarea Online 1: Explicación del uso de system.out.print y la variable int.

---
+++ src/TareaOnline01/Variable.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TareaOnline01;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Variable {
+    public static void main(String[] args) {
+        
+        // System.out.println(sout)se utiliza para imprimir por pantalla.
+        System.out.println("Juan Dámaso Pastor");
+        
+        /*La variable en Java es un espacio de memoria en la que
+        guardamos un determinado valor. Para crear una variable ENTERA 
+        usamos el tipo INT, luego le damos un nombre y despues un valor.
+        */
+        int num = 5;
+        System.out.println("El valor de int para esta tarea es: " + num);
+    }
+}
